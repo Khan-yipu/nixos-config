021781670f5dfb3ce481c6e891275c7f273864f4
cake <zhaocake@foxmail.com>
2026-03-19 20:30:11 +0800

    fix(nix-index): remove it from lock.

--- hosts/nixos-vm/configuration.nix
+++ hosts/nixos-vm/configuration.nix
@@ -95,11 +95,9 @@
     "https://mirrors.ustc.edu.cn/nix-channels/store"
     "https://mirror.sjtu.edu.cn/nix-channels/store"
     "https://cache.nixos.org"
-    "https://nix-community.cachix.org"
   ];
   nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
